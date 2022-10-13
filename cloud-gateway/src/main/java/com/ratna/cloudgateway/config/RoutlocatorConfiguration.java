3b884ffc20f8ca2ac6847e9a5eb98b2cfadb57b8
Ratna1992 <ratnasrinivas1992@gmail.com>
2022-10-14 00:27:49 +0530

    hystrix enabled

--- cloud-gateway/src/main/java/com/ratna/cloudgateway/config/RoutlocatorConfiguration.java
+++ cloud-gateway/src/main/java/com/ratna/cloudgateway/config/RoutlocatorConfiguration.java
@@ -1,12 +1,23 @@
 package com.ratna.cloudgateway.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+import org.springframework.cloud.gateway.filter.factory.SpringCloudCircuitBreakerFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.DispatcherHandler;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
-public class RoutlocatorConfiguration {
+public class RoutlocatorConfiguration extends SpringCloudCircuitBreakerFilterFactory {
+	public RoutlocatorConfiguration(ReactiveCircuitBreakerFactory reactiveCircuitBreakerFactory,
+			ObjectProvider<DispatcherHandler> dispatcherHandlerProvider) {
+		super(reactiveCircuitBreakerFactory, dispatcherHandlerProvider);
+	}
+
 	@Bean
 	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
@@ -19,6 +30,12 @@ public class RoutlocatorConfiguration {
 										c -> c.setName("payment-service").setFallbackUri("forward:/paymentFallBack")))
 								.uri("lb://payment-service"))
 				.build();
+	}
+
+	@Override
+	protected Mono<Void> handleErrorWithoutFallback(Throwable t, boolean resumeWithoutError) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
